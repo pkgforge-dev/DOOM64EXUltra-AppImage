db394402c14b755b9f8aa7f01faa9f22e3edabf8
Link4Electronics <link4electronics@gmail.com>
2026-03-05 17:46:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,22 +7,18 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake      \
-    fluidsynth \
-    libdecor   \
-    portmidi   \
-    sdl3
+    cmake          \
+    fluidsynth     \
+    pipewire-audio \
+    pipewire-jack  \
+    portmidi       \
+    sdl3_mixer
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-make-aur-package
-#make-aur-package sdl3_mixer-git
-
-# If the application needs to be manually built that has to be done down here
-echo "Making nightly build of DOOM64EXUltra..."
+echo "Building DOOM64EXUltra..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/StevenSYS/DOOM64EXUltra"
 VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
@@ -30,10 +26,6 @@ git clone --recursive --depth 1 "$REPO" ./DOOM64EXUltra
 echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
-cd ./DOOM64EXUltra
-cmake . \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_FLAGS="-Wno-implicit-function-declaration"
-make -j$(nproc)
-mv -v DOOM64EXUltra ../AppDir/bin
-mv -v doom64ex-ultra.wad ../AppDir/bin
+cmake -S ./DOOM64EXUltra -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-Wno-implicit-function-declaration"
+cmake --build build -j$(nproc)
+mv -v build/DOOM64EXUltra build/doom64ex-ultra.wad ./AppDir/bin
