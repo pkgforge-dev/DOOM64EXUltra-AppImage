9e5beada6f67dc3ca1358033b540f87e9b5dcbb8
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:55:03 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -20,10 +20,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package
-
-# If the application needs to be manually built that has to be done down here
 echo "Making nightly build of DOOM64EXUltra..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/StevenSYS/DOOM64EXUltra"
